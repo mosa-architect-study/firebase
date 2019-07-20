bdd28264e8da31890cc4fc3c894f47b8961ade29
hagaitski <i.haga629@gmail.com>
2019-07-20 15:36:53 +0900

    カウンター

--- haga/func-app/src/index.ts
+++ haga/func-app/src/index.ts
@@ -1,8 +1,30 @@
 import * as functions from 'firebase-functions';
+import *  as admin from 'firebase-admin';
+
+admin.initializeApp(functions.config().firebase);
+const store = admin.firestore();
+
+store.collection("vistors").doc("history").set({
+    count:0
+})
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.region("asia-northeast1").https.onRequest((request, response) => {
- response.send("芳賀の関数");
+export const visit = functions.region("asia-northeast1").https.onRequest((request, response) => {
+
+    const history = store.collection("vistors").doc("history")
+    
+    history.get().then(e => {
+        const data = e.data();
+        if(data){
+            history.set({
+                count:data.count + 1
+            })
+            response.json(e.data())
+        } else {
+            response.send("Data Not Found")
+        }
+        
+    })
 });
