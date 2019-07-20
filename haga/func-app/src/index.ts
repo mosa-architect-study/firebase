9931b8dd154ddb213c1a359110674b1dc9379c4a
hagaitski <i.haga629@gmail.com>
2019-07-20 22:25:08 +0900

    ローカルde

--- haga/func-app/src/index.ts
+++ haga/func-app/src/index.ts
@@ -1,31 +1,40 @@
 import * as functions from 'firebase-functions';
 import *  as admin from 'firebase-admin';
+import * as testing from "@firebase/testing"
 
-admin.initializeApp(functions.config().firebase);
-const store = admin.firestore();
+const createFirestore = () => {
+    if (process.env.FUNCTIONS_EMULATOR) {
+        const store = testing.initializeAdminApp({
+            projectId:"haga-number-of-vistors"
+        }).firestore()
+        // store.collection("vistors").doc("history").set({
+        //     count:0
+        // })
+        return store
+    } else {
+        admin.initializeApp()
+        return admin.firestore();
+    }
+   
+}
 
-store.collection("vistors").doc("history").set({
-    count:0
-})
+const store = createFirestore()
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const visit = functions.region("asia-northeast1").https.onRequest((request, response) => {
+export const visit = functions.region("asia-northeast1").https.onRequest(async (request, response) => {
 
     response.header('Access-Control-Allow-Origin', "*");
     response.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
     const history = store.collection("vistors").doc("history")
-    history.get().then(e => {
-        const data = e.data();
-        if(data){
-            history.set({
-                count:data.count + 1
-            })
-            response.json(e.data())
-        } else {
-            response.send("Data Not Found")
-        }
-        
-    })
+
+    const e = await history.get();
+    const data = e.data();
+    if(data){
+        history.set({
+            count:data.count + 1
+        })
+        response.json(e.data())
+    } else {
+        response.send("Data Not Found")
+    }
+
 });
